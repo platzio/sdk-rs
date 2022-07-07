4a0f14d34723bda00fc337926ab3612a55802e95
Zohar Zilberman <popen2@gmail.com>
2022-07-07 14:27:01 +0300

    Add feature flag for using uuid 1

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,3 +1,5 @@
 mod deployment_status;
+mod uuid;
 
 pub use deployment_status::*;
+pub use uuid::Uuid;
---
+++ src/types/uuid.rs
@@ -0,0 +1,5 @@
+#[cfg(feature = "uuid_08")]
+pub use uuid08::Uuid;
+
+#[cfg(feature = "uuid_1")]
+pub use uuid1::Uuid;
