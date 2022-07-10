c590ec61ab359cb3f50a58e3af7aafe2a1986b95
Zohar Zilberman <popen2@gmail.com>
2022-07-10 08:26:19 +0300

    Add Env resource

---
+++ src/resources/envs.rs
@@ -0,0 +1,34 @@
+use crate::client::PlatzClient;
+use crate::types::Uuid;
+use anyhow::Result;
+use chrono::prelude::*;
+use kv_derive::{prelude::*, IntoVec};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct Env {
+    pub id: Uuid,
+    pub created_at: DateTime<Utc>,
+    pub name: String,
+    pub node_selector: serde_json::Value,
+    pub tolerations: serde_json::Value,
+    pub auto_add_new_users: bool,
+}
+
+#[derive(Default, IntoVec)]
+pub struct EnvFilters {
+    #[kv(optional)]
+    pub name: Option<String>,
+    #[kv(optional)]
+    pub auto_add_new_users: Option<bool>,
+}
+
+impl PlatzClient {
+    pub async fn envs(&self, filters: EnvFilters) -> Result<Vec<Env>> {
+        Ok(self
+            .request(reqwest::Method::GET, "/api/v2/envs")
+            .add_to_query(filters.into_vec())
+            .paginated()
+            .await?)
+    }
+}
--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,9 +1,11 @@
 mod deployment_resource_types;
 mod deployment_resources;
 mod deployments;
+mod envs;
 mod k8s_clusters;
 
 pub use deployment_resource_types::*;
 pub use deployment_resources::*;
 pub use deployments::*;
+pub use envs::*;
 pub use k8s_clusters::*;
